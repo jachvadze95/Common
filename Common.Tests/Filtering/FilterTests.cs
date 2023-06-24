6fd475552c514021051a97de38ea77eaea7e176d
jachvadze <jachvadze@yahoo.com>
2023-06-25 01:31:39 +0400

    removed extra stuff

--- Common.Tests/Filtering/FilterTests.cs
+++ Common.Tests/Filtering/FilterTests.cs
@@ -5,12 +5,6 @@ namespace Common.Tests.Filtering
     [TestClass]
     public class UnitTest1
     {
-        // to have the same Configuration object as in Startup
-        private IConfigurationRoot _configuration;
-
-        // represents database's configuration
-        private DbContextOptions<CarServiceContext> _options;
-
         public UnitTest1()
         {
             
